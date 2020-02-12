8c65e3a54819ab787a0edd9a4aa03e2a8d9e82a0
Owen Klijnsma <35454653+owenklijnsma@users.noreply.github.com>
2020-02-12 10:55:26 +0100

    finished week1\ex1

---
+++ 1920/web_scripting/week1/excercises/ex1/triangle.js
@@ -0,0 +1,73 @@
+//Global variable
+let amountOfLines = 7;
+
+//Triangle A
+console.log('Triangle A:');
+
+for (let i = 1; i <= amountOfLines; i++) {
+    let row = '';
+    for (let j = 1; j <= i; j++) {
+        row += '#\t';
+    }
+
+    console.log(row);
+}
+
+console.log('');
+
+//Triangle B
+console.log('Triangle B:');
+
+for (let i = 1; i <= amountOfLines; i++) {
+    let row = '';
+    for (let j = 1; j <= (amountOfLines - i); j++) {
+        row += '\t';
+    }
+    for (let k = 1; k <= i; k++) {
+        row += '#\t';
+    }
+    console.log(row);
+}
+
+console.log('');
+
+//Triangle C
+console.log('Triangle C:');
+
+for (let i = 0; i < amountOfLines; i++) {
+    let row = '';
+    for (let j = 1; j < amountOfLines - i; j++) {
+        row = row + '\t';
+    }
+    for (let k = 1; k <= (2 * i + 1); k++) {
+        row = row + '#\t';
+    }
+    console.log(row);
+}
+
+console.log('');
+
+//Triangle D
+console.log('Triangle D:');
+
+let interval = 5;
+let christmasTree = '';
+let count = 5;
+
+for (let i = 0; i < amountOfLines; i++) {
+    let row = '';
+    for (let j = 1; j < amountOfLines - i; j++) {
+        row = row + '\t';
+    }
+    for (let k = 1; k <= (2 * i + 1); k++) {
+        if(count % 5 === 0){
+            row = row + '@\t';
+        } else {
+            row = row + '#\t';
+        }
+        count++;
+    }
+    christmasTree += (row + '\n')
+}
+
+console.log(christmasTree);
